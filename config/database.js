a4f59692b3626760fdfe5917be8f6c390e14e6b4
jdavidfa0 <jdavidfa0@gmail.com>
2024-07-19 11:44:37 -0500

    se conecta la api con la db de AWS

--- config/database.js
+++ config/database.js
@@ -1,9 +1,9 @@
 const db={
-    database: 'railway',
-    host: 'monorail.proxy.rlwy.net',
-    password: 'LAGCjOAQCMEbbiRBiHOFnbssYWUlvoNm',
-    port: 28311,
-    user: 'root',
+    database: 'db_spac',
+    host: 'dbspac.cb8i062mmrzs.us-east-2.rds.amazonaws.com',
+    password: 'crisvalencia456',
+    port: 3306,
+    user: 'admin',
 }
 
 export default db  

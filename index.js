4acbb8144e3ebb60f00dc0092448b13e33065107
mrpatosenpai <garciacacher@gmail.com>
2024-07-30 14:49:05 -0500

    entradas del diario12

--- index.js
+++ index.js
@@ -1,28 +1,33 @@
 import express from 'express';
 import session from 'express-session';
-import FileStore from 'session-file-store';
+import MySQLStore from 'express-mysql-session';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import routes from './config/routes.js';
 
 const app = express();
-const FileStoreInstance = FileStore(session);
-// Configuración de CORS
-const corsOptions = {
-    origin: '*', 
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true, 
-};
 
-// Configuración de la sesión
 app.use(session({
-    store: new FileStoreInstance(),
     secret: 'crisvalencia456',
     resave: false,
-    saveUninitialized: true,
-    cookie: { secure: true } 
-}));
+    saveUninitialized: false,
+    cookie: {
+        secure: process.env.NODE_ENV === 'production', // Usa 'true' si estás en producción y tu app usa HTTPS
+        httpOnly: true,
+        sameSite: 'lax' // Cambia según sea necesario
+    },
+    store: new MySQLStore({
+        expiration: 86400000, // Tiempo de expiración en milisegundos (1 día)
+        createDatabaseTable: true,
+    }, mysql.createPool(db))
+}))
+
+const corsOptions = {
+    origin: '*', // Cambia esto por tu dominio o usa '*' para desarrollo
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true, // Habilita el soporte de cookies
+};
 
 // Configuración del middleware
 app.use(cors(corsOptions)); 

71032dcfd8c999320801bb49d26cb214998f94ca
mrpatosenpai <garciacacher@gmail.com>
2024-08-01 14:52:22 -0500

    imprimir diario32

--- index.js
+++ index.js
@@ -18,34 +18,39 @@ app.use(session({
     saveUninitialized: true,
     cookie: {
         maxAge: 86400000, // 24 horas
-        secure: false, // Cambia a true si estás utilizando HTTPS
+        secure: true, // Debe ser true para HTTPS
         httpOnly: true,
-        sameSite: 'None' // Asegúrate de que las cookies se envíen en solicitudes de terceros
+        sameSite: 'None',
     }
 }));
+
 app.use((req, res, next) => {
     console.log(`${req.method} ${req.url}`);
     next();
 });
+
 // Configuración de CORS
 const corsOptions = {
-    origin: 'http://apispac-production.up.railway.app/', // Reemplaza con el origen de tu cliente
+    origin: 'https://apispac-production.up.railway.app', // Asegúrate de que coincida con el origen de tu cliente
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
+    credentials: true // Permite el envío de cookies
 };
 
-app.use(cors(corsOptions)); 
+app.use(cors(corsOptions));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// Rutas principales
 app.use('/api', routes);
 
+// Ruta de prueba para verificar la sesión
 app.get('/api/usuarios/test', (req, res) => {
     console.log('Session on test route:', req.session);
     res.json(req.session);
 });
 
+// Ruta principal
 app.get('/', (req, res) => res.send('Bienvenidos a mi API :D'));
 
 const server = app.listen(process.env.PORT || 8000, () => {
